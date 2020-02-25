699f65f3e02fe522f240d872566127b20fd52605
Roman <romabukhtiarov@gmail.com>
2020-02-25 09:59:33 +0300

    Proceed insert user errors

--- crud.php
+++ crud.php
@@ -4,24 +4,18 @@ $TYPE = $_GET["TYPE"];
 
 $link = mysqli_connect("localhost", "id12603992_admin", "admin", "id12603992_civitati");
 
+//Register User
 if (strcmp($TYPE, "I") == 0 ) {
 	$NICKNAME = $_GET["NICKNAME"];
     $PASSWORD = $_GET["PASSWORD"];
 	$sql = "INSERT INTO USERS (NICKNAME, PASSWORD) VALUES ('" . $NICKNAME . "','" . $PASSWORD . "')";
-	$link->query($sql);
-	echo "New record created successfully";
-}
-
-/*
-
-echo($sql);
-
 if ($link->query($sql) === TRUE) {
     echo "New record created successfully";
 } else {
     echo "Error: " . $sql . "<br>" . $link->error;
 }
-*/
+}
+
 
 mysqli_close($link);
 ?>
